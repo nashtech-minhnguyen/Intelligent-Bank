740fde27f45a24d8ddd390a54c4c0674161acc62
Minh Nguyen Ngoc <minhnguyenn5@nashtechglobal.com>
2024-01-09 11:13:39 +0700

    [IB] Update Transaction Entity

--- src/main/java/com/softbank/accountmanagment/entity/Account.java
+++ src/main/java/com/softbank/accountmanagment/entity/Account.java
@@ -4,11 +4,14 @@ import lombok.Data;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.math.BigDecimal;
+import java.util.List;
 
 @Entity
 @Table(name = "accounts")
@@ -22,4 +25,7 @@ public class Account {
 
   @Column(name= "account_balance")
   private BigDecimal accountBalance;
+
+  @OneToMany(mappedBy = "accounts", fetch = FetchType.LAZY)
+  private List<Transaction> transactions;
 }
--- src/main/java/com/softbank/accountmanagment/entity/Transaction.java
+++ src/main/java/com/softbank/accountmanagment/entity/Transaction.java
@@ -1,9 +1,11 @@
 package com.softbank.accountmanagment.entity;
 
 import lombok.Data;
+import lombok.NonNull;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -11,6 +13,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.math.BigDecimal;
 
 @Entity
 @Data
@@ -22,8 +25,14 @@ public class Transaction {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @JoinColumn(name = "account_id")
-    @ManyToOne
-    private Account account;
+    @JoinColumn(name = "source_account_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Account sourceAccount;
+
+    @Column(name = "target_account_id")
+    private Long targetAccountId;
+
+    @Column(name = "amount")
+    private BigDecimal amount;
 
 }
